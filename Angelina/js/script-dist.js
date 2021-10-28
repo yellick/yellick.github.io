fdd2b1a2a4614fdf85902fe3d09b7d1292908d95
yellick <85665725+yellick@users.noreply.github.com>
2021-10-28 14:17:51 +0500

    Add files via upload

--- Angelina/js/script-dist.js
+++ Angelina/js/script-dist.js
@@ -7,20 +7,20 @@ $(document).ready(() => {
 // функция по нажатию на кнопку
 $('.menu-btn').on('click', function (e) {
     // добавляю кнопке класс делающей её "крестиком"
-    $('.menu-btn').toggleClass('menu-btn_active');
+    $('.menu-btn').addClass('menu-btn_active');
     // отображаю заливку страницы с помощью класса
-    $('.fill').toggleClass('fill--active');
+    $('.fill').addClass('fill--active');
     $('.fill').css('opacity', '1')
     // выдвигаю сайдбар меню
-    $('.menu').toggleClass('menu--show');
+    $('.menu').addClass('menu--show');
 });
 // деактивация кнопки меню
 // функция по нажатию на заливку
 $('.fill').on('click', function () {
     // делаю кнопку меню обычной
-    $('.menu-btn').toggleClass('menu-btn_active');
+    $('.menu-btn').removeClass('menu-btn_active');
     // выдвигаю сайдбар меню
-    $('.menu').toggleClass('menu--show');
+    $('.menu').removeClass('menu--show');
     $('.fill').css('opacity', '0')
     setTimeout(() => {
         // убираю заливку
