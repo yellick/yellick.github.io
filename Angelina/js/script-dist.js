89c4dda74ab1dfe404bc8493cf5d9c2ca9ac517a
yellick <85665725+yellick@users.noreply.github.com>
2021-11-01 17:52:21 +0500

    Add files via upload

--- Angelina/js/script-dist.js
+++ Angelina/js/script-dist.js
@@ -27,3 +27,42 @@ $('.fill').on('click', function () {
         $('.fill').removeClass('fill--active');
     }, 200)
 });
+// функция по нажатию на пункт меню
+$('.menu_item').on('click', function () {
+    // делаю кнопку меню обычной
+    $('.menu-btn').removeClass('menu-btn_active');
+    // выдвигаю сайдбар меню
+    $('.menu').removeClass('menu--show');
+    $('.fill').css('opacity', '0')
+    setTimeout(() => {
+        // убираю заливку
+        $('.fill').removeClass('fill--active');
+    }, 200)
+});
+
+// скролл по кнопке
+$('a[href^="#"]').click(function () {
+    let valHref = $(this).attr("href");
+    $('html, body').animate({
+        scrollTop: $(valHref).offset().top - 75
+    })
+});
+
+// изменение активного пункта меню
+$(window).scroll(() => {
+    let scrollDistance = $(window).scrollTop();
+
+    $('.paragraph').each((i, el) => {
+        if ($(el).offset().top - $('header').outerHeight() <= scrollDistance + ($(window).height() * 0.5)) {
+
+            $('nav a span').each((i, el) => {
+
+                if ($(el).hasClass('menu_item--active')) {
+                    $(el).removeClass('menu_item--active');
+                }
+            });
+
+            $('nav a:eq(' + i + ')').find('span').addClass('menu_item--active');
+        };
+    });
+});
