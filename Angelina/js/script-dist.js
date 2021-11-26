902bda01ca0c770bc310ae2b7e39a5f493273c92
yellick <85665725+yellick@users.noreply.github.com>
2021-11-26 20:20:29 +0500

    Add files via upload

--- Angelina/js/script-dist.js
+++ Angelina/js/script-dist.js
@@ -7,7 +7,7 @@ $(document).ready(() => {
 
 // активация кнопки меню
 // функция по нажатию на кнопку
-$('.menu-btn').on('click', function (e) {
+$('.menu-btn--o').on('click', function () {
     // добавляю кнопке класс делающей её "крестиком"
     $('.menu-btn').addClass('menu-btn_active');
     // отображаю заливку страницы с помощью класса
@@ -16,14 +16,19 @@ $('.menu-btn').on('click', function (e) {
     // выдвигаю сайдбар меню
     $('.menu').addClass('menu--show');
 });
+$('.menu-btn--c').on('click', function(){
+    $('.menu-btn').removeClass('menu-btn_active');
+    $('.menu').removeClass('menu--show');
+    $('.fill').css('opacity', '0');
+});
 // деактивация кнопки меню
 // функция по нажатию на заливку
-$('.fill').on('click', function () {
+$('.fill').on('click', function(){
     // делаю кнопку меню обычной
     $('.menu-btn').removeClass('menu-btn_active');
     // выдвигаю сайдбар меню
     $('.menu').removeClass('menu--show');
-    $('.fill').css('opacity', '0')
+    $('.fill').css('opacity', '0');
     setTimeout(() => {
         // убираю заливку
         $('.fill').removeClass('fill--active');
@@ -31,9 +36,7 @@ $('.fill').on('click', function () {
 });
 // функция по нажатию на пункт меню
 $('.menu_item').on('click', function () {
-    // делаю кнопку меню обычной
     $('.menu-btn').removeClass('menu-btn_active');
-    // выдвигаю сайдбар меню
     $('.menu').removeClass('menu--show');
     $('.fill').css('opacity', '0')
     setTimeout(() => {
@@ -70,9 +73,10 @@ $(window).scroll(() => {
     
     if (scrollDistance >= $('.title').height()) {
         $('.header').addClass('header--active')
-    } else if (scrollDistance <= 400) {
+    } else {
         $('.header').removeClass('header--active')
     };
+   
 });
 
 // работа слайдера
