655d0bf92905bb4c8d43b53e800e956fc74cc500
yellick <85665725+yellick@users.noreply.github.com>
2022-01-07 13:11:52 +0500

    Add files via upload

--- Tester/js/script-dist.js
+++ Tester/js/script-dist.js
@@ -1,54 +1,117 @@
-$('#open-sett-menu-btn').on('click', function() {
+// открытие и закрытие модального окна
+function openSettingsWindow() {
     $('.settings-window').css('opacity', '1');
     $('.settings-window').addClass('settings-window-active');
-});
-$('#close-settins-btn').on('click', function() {
+};
+
+function closeSettingsWindow() {
     $('.settings-window').css('opacity', '0');
     setTimeout(() => {
         $('.settings-window').removeClass('settings-window-active');
     }, 300);
+};
+
+// открытие/закрытие окна с настройками поиска
+$('#open-sett-menu-btn').on('click', openSettingsWindow);
+$('#close-settins-btn').on('click', closeSettingsWindow);
+
+//
+$('#sortTestList-btn').on('click', function () {
+    let testList = document.getElementById('tests-list');
+    // удаляю все тесты из списка
+    testList.innerHTML = '';
+    generatingTestsList();
+    closeSettingsWindow();
 });
 
-$('.class-list-el').click(function(){
-    $('.class-list-el').removeClass('class-list-el-active');
-    $(this).addClass('class-list-el-active');
-    // variable = this.value      получение номера кнопки
-});
+// устанавливаю Название странице
+function setPageTitle() {
+    document.querySelector('title').textContent = siteName;
+};
 
-function makingTestsList() {
+// генерация списка тестов
+function generatingTestsList() {
     let list = document.getElementById('tests-list');
-    
+
+    let searchClass = request[0];
+    let searchLesson = request[1];
+
     tests.forEach((item, i) => {
-        
-        // создаю элемент
-        let listElement = document.createElement('a');
-        let lessonName = document.createElement('p');
-        let aboutLesson = document.createElement('div');
-        let lesson = document.createElement('span');
-        let dash = document.createElement('span');
-        let lessonClass = document.createElement('span');
-        
-        // задаю для каждого из созданных элементов класс
-        listElement.classList.add('list-elem');
-        lessonName.classList.add('lesson-name');
-        aboutLesson.classList.add('about-lesson');
-        
-        listElement.setAttribute('href', 'lesson/index.html#' + i)
-        
-        // присваиваю контент
-        lessonName.textContent = item['name'];
-        lesson.textContent = item['lessonName'];
-        dash.textContent = ' - ';
-        lessonClass.textContent = item['class'] + 'класс';
-        
-        // добавляю элементы на страницу
-        list.appendChild(listElement);
-        listElement.appendChild(lessonName);
-        listElement.appendChild(aboutLesson);
-        aboutLesson.appendChild(lesson);
-        aboutLesson.appendChild(dash);
-        aboutLesson.appendChild(lessonClass);
+
+        // проверяю тест на соответствие запросу
+        if ((item['class'] == searchClass || searchClass == 'all') && (item['lesson'] == searchLesson || searchLesson == 'all')) {
+            
+            // создаю элемент
+            let listElement = document.createElement('a');
+            let lessonName = document.createElement('p');
+            let aboutLesson = document.createElement('div');
+            let lesson = document.createElement('span');
+            let dash = document.createElement('span');
+            let lessonClass = document.createElement('span');
+
+            // задаю для каждого из созданных элементов класс
+            listElement.classList.add('list-elem');
+            lessonName.classList.add('lesson-name');
+            aboutLesson.classList.add('about-lesson');
+
+            listElement.setAttribute('href', 'lesson/index.html#' + i)
+
+            // присваиваю контент
+            lessonName.textContent = item['name'];
+            lesson.textContent = item['lessonName'];
+            dash.textContent = ' - ';
+            lessonClass.textContent = item['class'] + ' класс';
+
+            // добавляю элементы на страницу
+            list.appendChild(listElement);
+            listElement.appendChild(lessonName);
+            listElement.appendChild(aboutLesson);
+            aboutLesson.appendChild(lesson);
+            aboutLesson.appendChild(dash);
+            aboutLesson.appendChild(lessonClass);
+        }
     })
 };
 
-makingTestsList()
+// генерирую список предмтов 
+function generatingLessonsList() {
+    let lessonsList = document.getElementById('lessons-list');
+
+    lessons.forEach((i, el) => {
+        // i - элемент массива
+        let opt = document.createElement('option');
+        opt.textContent = i['lessonName'];
+        opt.setAttribute('value', i['lesson']);
+        lessonsList.appendChild(opt);
+    });
+};
+
+// получение значиние с селекта и кнопок
+function getValuesFromSearchSettings() {
+    let selectedLesson;
+    let selectedClass;
+
+    // получаю значение с выбора класса
+    $('.class-list-el').click(function () {
+        $('.class-list-el').removeClass('class-list-el-active');
+        $(this).addClass('class-list-el-active');
+        selectedClass = this.value;
+        request[0] = selectedClass;
+    });
+
+    // получаю значение с выбора урока
+    $('#lessons-list').change(function () {
+        selectedLesson = this.value;
+        request[1] = selectedLesson;
+    });
+};
+
+
+// переменная с запросом для сортировки списка тестов
+// request[0] - класс; request[1] - урок
+let request = ['all', 'all'];
+
+setPageTitle()
+generatingTestsList(request);
+generatingLessonsList();
+getValuesFromSearchSettings()
--- Tester/lesson/js/script-dist.js
+++ Tester/lesson/js/script-dist.js
@@ -1,9 +1,11 @@
+// даю заголовок странице
 function setPageTitle(lessonName) {
     // определяю тег title
     // устанавливаю в него содержимое с названием сайта и темой урока
     document.querySelector('title').textContent = siteName + ' | ' + lessonName
 }
 
+// создаю тест
 function setTestContent(lessonIndex) {
     // определяю заголовки и задаю им контент
     document.getElementById('title').textContent = tests[lessonIndex]['name'];
@@ -23,7 +25,7 @@ function setTestContent(lessonIndex) {
         let questionNumberContent = document.createElement('p');
 
         // заполняю вопросы теста
-        let answerOptions = document.createElement('div');
+        let answerOptions = document.createElement('form');
         // индекс вопроса для генерации id
         let questionIndex = el;
         i['answers'].forEach((i, el) => {
@@ -33,14 +35,15 @@ function setTestContent(lessonIndex) {
             let answerInputLabel = document.createElement('label');
             
             // создаю общее значение атрибутов id и for
-            let id = 'answer-' + (questionIndex + 1) + '-' + (el + 1)
+            let id = 'answer-' + (questionIndex) + '-' + (el)
 
             // добавляю классы
             answerOption.classList.add('answer-option');
             
             // устанавливаю атрибуты
             answerInput.setAttribute('type', 'radio');
-            answerInput.setAttribute('name', 'answer-' + (questionIndex + 1));
+            answerInput.setAttribute('class', 'answer');
+            answerInput.setAttribute('name', 'answer');
             answerInput.setAttribute('id', id);
             answerInput.setAttribute('value', i)
             
@@ -62,7 +65,6 @@ function setTestContent(lessonIndex) {
         answerOptions.classList.add('answer-options');
         
         // добавляю контент
-        questionText.textContent = i['question'];
         questionNumberContent.textContent = el + 1;
 
         // добавляю элементы на страницу
@@ -72,7 +74,14 @@ function setTestContent(lessonIndex) {
         questionText.appendChild(questionNumber);
         questionNumber.appendChild(questionNumberContent);
         questionText.appendChild(questionTextContent);
+        questionTextContent.innerHTML = i['question'];
     });
+    
+    
+    let question = document.createElement('div');
+    question.classList.add('question');
+    test.appendChild(question);
+    
     
     // создаю кнопку отправки
     let submitBtn = document.createElement('button');
@@ -82,10 +91,54 @@ function setTestContent(lessonIndex) {
     test.appendChild(submitBtn);
     
     // функция по нажатию кнопки отправки
-    $('#submit-btn').on('click', function(){
-        
-    })
+    $('#submit-btn').on('click', function(e){
+        e.preventDefault()
+        checkingTest(tests[lessonIndex]['answers'])
+    });
 };
+
+// проверяю правильность теста
+function checkingTest(rightAnswers){
+    let rightAnswersCount = 0;
+    let userAnswers = [];
+    
+    // перебираю все выделенные инпуты
+    for (let i = 0; i < $(".answer-options input:checked").length; i++) {
+        // получаю значение id, разбиваю его на до и после
+        let dataFromId = $(".answer-options input:checked")[i].getAttribute('id').slice(7).split('-');
+        // dataFromId[0] - номер вопроса, dataFromId[1] - номер ответа
+        // добавляю ответы в массив по их индексу
+        userAnswers[dataFromId[0]] = dataFromId[1];
+    };
+    
+    // сравниваю ответы
+    for (let i = 0; i < userAnswers.length; i++) {
+        if (userAnswers[i] == rightAnswers[i]){
+            rightAnswersCount++;
+        };
+    };
+    
+    setWindowResult(rightAnswersCount);
+};
+
+// заполняю окошко результатов
+function setWindowResult(rightAnswersCount) {
+    let questionCount = tests[lessonIndex]['questions'].length;
+    let assigment = rightAnswersCount / questionCount;
+            
+    if (assigment < 0.6) assigment = 2;
+    if (0.8 > assigment >= 0.6) assigment = 3;
+    if (1 > assigment >= 0.8) assigment = 4;
+    if (1 == assigment) assigment = 5;
+    
+    document.getElementById('test-theme').textContent += tests[lessonIndex]['name'];
+    document.getElementById('assessment-from-site').textContent += assigment;
+    document.getElementById('rigth-answers-count').textContent += rightAnswersCount + '/' + questionCount;
+    
+    $('.result-window-container').css({'left': '0', 'opacity': '1'});
+    $('body').css('overflow-y', 'hidden');
+};
+
 
 // получаю хеш страницы, отбрасываю первый символ строки (#), конвертирую строку в число и вызываю с ним функцию.
 // document.location - ссылка на эту страницу
